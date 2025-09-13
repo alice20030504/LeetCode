b377b0cca7c46ef1b192e551fad11431c6d1445c
alice <alice0coche@gmail.com>
2025-09-13 02:25:23 -0400

    LC-trapping-rain-water [Runtime: 0 ms; Memory: 46.6 MB]

---
+++ array/trapping-rain-water.java
@@ -0,0 +1,33 @@
+// Two Pointers:
+// Use left and right pointers.
+// Maintain left_max and right_max.
+// Move the smaller side inward, accumulate trapped water.
+
+// Time: O(n), Space: O(1).
+
+public class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+}
