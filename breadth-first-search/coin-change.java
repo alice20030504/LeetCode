c25a39a9c19078bd12f88e54a67cea82e6f2870b
alice <alice0coche@gmail.com>
2025-09-29 14:30:29 -0400

    LC-coin-change [Runtime: 15 ms; Memory: 44.6 MB]

---
+++ breadth-first-search/coin-change.java
@@ -0,0 +1,21 @@
+// Classic unbounded knapsack / coin change problem.
+
+import java.util.*;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1; // sentinel for "infinity"
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == max ? -1 : dp[amount];
+    }
+}
