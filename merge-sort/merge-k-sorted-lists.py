7d2a27437a71b14adb85cb9007a1a4b4cea97aa6
alice <alice0coche@gmail.com>
2025-11-15 13:12:02 -0500

    LC-merge-k-sorted-lists [Runtime: 8 ms; Memory: 20.5 MB]

--- merge-sort/merge-k-sorted-lists.py
+++ merge-sort/merge-k-sorted-lists.py
@@ -1,30 +1,29 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 import heapq
+
+# Definition for singly-linked list.
 class ListNode:
-    def _init_(self, val=0, next=None):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    def mergeKLists(self, lists):
         min_heap = []
-
+        
+        # Push first node of each list into heap
         for index, node in enumerate(lists):
             if node:
-                heapq.heappush(min_heap,(node.val,index,node))
+                heapq.heappush(min_heap, (node.val, index, node))
+        
         dummy = ListNode()
         curr = dummy
-
+        
         while min_heap:
             val, index, node = heapq.heappop(min_heap)
             curr.next = node
             curr = curr.next
-
+            
             if node.next:
-                heapq.heappush(min_heap,(node.next.val, index, node.next))
-
-        return dummy.next        
+                heapq.heappush(min_heap, (node.next.val, index, node.next))
+        
+        return dummy.next
