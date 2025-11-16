d9b49d2fcf5652cb79c95fe3ce7d52f0c6690215
alice <alice0coche@gmail.com>
2025-11-16 14:55:17 -0500

    LC-next-permutation [Runtime: 0 ms; Memory: 17.8 MB]

--- two-pointers/next-permutation.py
+++ two-pointers/next-permutation.py
@@ -7,7 +7,7 @@ class Solution:
         while i >= 0 and nums[i] >= nums[i+1]:
             i -= 1
         if i >= 0:
-            j = len(nums) -1
+            j = len(nums) - 1
             while nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
