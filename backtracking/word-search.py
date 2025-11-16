4b03f68aa3bdea4a519602744b132956886e9e06
alice <alice0coche@gmail.com>
2025-11-16 14:32:10 -0500

    LC-word-search [Runtime: 3207 ms; Memory: 18 MB]

--- backtracking/word-search.py
+++ backtracking/word-search.py
@@ -3,22 +3,24 @@ class Solution:
         # return
         m,n = len(board), len(board[0])
         def dfs(x,y, idx) -> bool:
+            #在这里写的是退出条件1，即遍历结束返回答案的条件
             if idx == len(word):
                 return True
-            #
+            #在这里写的是退出条件2，即放弃一个branch的条件
             if x<0 or y<0 or x>=m or y>=n or board[x][y] != word[idx]:
                 return False
-
+            #标记这个point为访问过【暂时】
             temp = board[x][y]
             board[x][y] = '#'
 
-            #
+            #如果这个point符合条件，找这个point附近的下一个point
             found = (
                 dfs(x+1,y,idx+1) or
                 dfs(x,y+1,idx+1) or
                 dfs(x-1,y,idx+1) or
                 dfs(x,y-1,idx+1))
 
+            #如果到这一步说明所有的loop都返回found = True
             board[x][y]=temp
             return found
 
